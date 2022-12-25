293ff2df6a78acb074212243f000d941edb2fb9e
SIGRYSMA20670002 <rysmacantika532@gmail.com>
2022-12-25 16:02:45 +0700

    first commit

---
+++ qgisweb/data/Negeri_1.js
@@ -0,0 +1,13 @@
+var json_Negeri_1 = {
+"type": "FeatureCollection",
+"name": "Negeri_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Negeri": "SMK NEGERI 1 PATI", "Alamat": "Jl. A Yani No. 2 Pati", "Telepon": "0295381959", "Jurusan": "Akuntansi , Administrasi, Perkantoran, Pemasaran", "Email": "smknegeri1pati@yahoo.com" }, "geometry": { "type": "Point", "coordinates": [ 111.033455993505171, -6.753497532475771 ] } },
+{ "type": "Feature", "properties": { "Negeri": "SMK NEGERI 3 PATI", "Alamat": "Jl. Kolonel Sunandar 108 Kab. Pati", "Telepon": "0295381027", "Jurusan": "Akomodasi Perhotelan, Tata Boga, Tata Kecantikan, Tata Busana", "Email": "smkn3pati@yahoo.co.id" }, "geometry": { "type": "Point", "coordinates": [ 111.057392858023888, -6.749145375290551 ] } },
+{ "type": "Feature", "properties": { "Negeri": "SMK NEGERI 4 PATI", "Alamat": "Jl. Rendole No. 1 Pati", "Telepon": "0295392257", "Jurusan": "Teknik Kapal Penangkap Ikan", "Email": "smkn4pati@gmail.com" }, "geometry": { "type": "Point", "coordinates": [ 111.001902853912327, -6.750233414586856 ] } },
+{ "type": "Feature", "properties": { "Negeri": "SMK NEGERI 2 PATI", "Alamat": "Jl. Gembong Km 4 Rendole Tromol Pos 5", "Telepon": "0295392372", "Jurusan": "Teknik Gambar Bangunan, Teknik Otomasi Industri, Teknik Audio Video, Teknik Pemesinan, Teknik Otomotif Kendaraan Ringan, Multimedia", "Email": "smkn02pati@yahoo.co.id" }, "geometry": { "type": "Point", "coordinates": [ 110.987758343060364, -6.792666947142751 ] } },
+{ "type": "Feature", "properties": { "Negeri": "SMK NEGERI 1 CLUWAK", "Alamat": "Jl. Tayu - Jepara Km 15", "Telepon": "02914277788", "Jurusan": "Teknik Elektronika Industri, Teknik Otomotif Sepeda Motor,Multimedia", "Email": "smkn.cluwak@gmail.com" }, "geometry": { "type": "Point", "coordinates": [ 110.952941085578615, -6.51086476939976 ] } },
+{ "type": "Feature", "properties": { "Negeri": "SMK NEGERI JATENG", "Alamat": "Jl. Raya Pti - Tlogowungu Km 3", "Telepon": "0295292176", "Jurusan": "Teknik Perbaikan Bodi Otomotif, Teknologi Pengolahan Hasil Pertanian", "Email": "smknjawatengahpati@hotmail.com" }, "geometry": { "type": "Point", "coordinates": [ 110.997550696727117, -6.733912825142282 ] } }
+]
+}
